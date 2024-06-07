22ea8f2a8a89916575f4a0cf3d83acb56b7b93b2
Longspur Technologie PVT LTD <155938377+longspurtechnologies@users.noreply.github.com>
2024-06-07 23:06:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+api_base = "https://praful-chatbot1.openai.azure.com/openai/deployments/first-chatbot/extensions/chat/completions?api-version=2024-02-15-preview" # Add your endpoint here
+api_key = "f4c34d2b20dc48449ce4ba52f8015431" # Add your OpenAI API key here
+deployment_id = "first-chatbot"
+
+# Azure AI Search setup
+search_endpoint = "https://chatbot-aipaid.search.windows.net"; # Add your Azure AI Search endpoint here
+search_key = "1L7uKOtrSJdZL3Qz7BeJsnGy5B5IeIXjhXUrjnHZTnAzSeARWNQA" # Add your Azure AI Search admin key here
+search_index_name = "idx2"; # Add your Azure AI Search index name here
